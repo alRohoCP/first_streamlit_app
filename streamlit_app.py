965d138fb0442a4a220a78fe76a9bec3cfb11f7e
alRohoCP <78201363+alRohoCP@users.noreply.github.com>
2022-04-20 16:49:40 +0200

    Update streamlit_app.py
    Added the fruit selector in the dataframe for smoothies

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,11 @@
+# Libraries
 import streamlit
 import pandas
 
+# Imported files
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+
+# Streamlit Code
 streamlit.title('My Mom\'s New Healthy Diner')
 
 streamlit.header('Breakfast Menu')
@@ -11,5 +16,11 @@ streamlit.text('🥑🍞 Avocado Toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
-my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+
+#Interaction with the table to pick the fruits customers want to include
+streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
+
+#Smoothies table 
 streamlit.dataframe(my_fruit_list)
+
+
