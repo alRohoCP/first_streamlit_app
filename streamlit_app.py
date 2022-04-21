ef611efadf12ea742dd546d2c880bf10aea5d8cf
alRohoCP <78201363+alRohoCP@users.noreply.github.com>
2022-04-21 15:14:17 +0200

    Update streamlit_app.py
    Adding API calls to fruityvice

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,11 +1,15 @@
 # Libraries
 import streamlit
 import pandas
+import requests
 
 # Imported files
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 # We want to see the number of the fruits as id_row or index, not a numerical ID
 my_fruit_list = my_fruit_list.set_index('Fruit')
+
+# Fruitvyce api respones
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 
 # Streamlit Code
 streamlit.title('My Mom\'s New Healthy Diner')
@@ -27,4 +31,6 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 #Smoothies table 
 streamlit.dataframe(fruits_to_show)
 
+#API response
+streamlit.text(fruityvice_response)
 
